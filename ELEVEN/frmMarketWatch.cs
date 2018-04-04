06101631229a793a79a8b33a11438a33e2dbbfff
Ashwani <prince.chopra@kindlebit.com>
2018-04-04 13:22:58 +0530

    changes

--- ELEVEN/frmMarketWatch.cs
+++ ELEVEN/frmMarketWatch.cs
@@ -272,7 +272,7 @@ namespace ELEVEN
                       
                     }
                 }
-                dispatcherTimer.Interval = new TimeSpan(0, 0, 3);
+               // dispatcherTimer.Interval = new TimeSpan(0, 0, 3);
             }
             else
             {
