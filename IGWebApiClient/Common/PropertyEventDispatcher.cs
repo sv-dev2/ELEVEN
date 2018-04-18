34c24930a98b09a55411a89a870d48fdc0616c38
Ashwani <prince.chopra@kindlebit.com>
2018-04-18 17:43:02 +0530

    IGMarket Library Added

---
+++ IGWebApiClient/Common/PropertyEventDispatcher.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace IGWebApiClient.Common
+{
+    public interface PropertyEventDispatcher
+    {
+        void BeginInvoke(Action a);
+
+        void addEventMessage(string message);
+    }
+}
+
