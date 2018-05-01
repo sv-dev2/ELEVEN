6034e7841ab454f57e96776ab1992c1e4ebf0b72
Ashwani <prince.chopra@kindlebit.com>
2018-05-01 17:37:41 +0530

    Changes

--- ELEVEN.Models/BitFinix/BitfinexTimeframe.cs
+++ ELEVEN.Models/BitFinix/BitfinexTimeframe.cs
@@ -6,7 +6,7 @@ using System.Threading.Tasks;
 
 namespace ELEVEN.Models
 {
-    public class BitTimeframe
+    public class ClsTimeframe
     {
         public string Text { get; set; }
         public string Value { get; set; }
--- ELEVEN.Services/clsComman.cs
+++ ELEVEN.Services/clsComman.cs
@@ -68,21 +68,41 @@ namespace ELEVEN.Services
         }
 
 
-        public static List<BitTimeframe> GetBitTimeFrame()
+        public static List<ClsTimeframe> GetBitTimeFrame()
         {
-            var timeFrame = new List<BitTimeframe>() {
-                                new BitTimeframe {Text="one minute",Value="1m" },
-                                new BitTimeframe {Text="five minutes",Value="5m" },
-                                new BitTimeframe {Text="15 minutes",Value="15m" },
-                                new BitTimeframe {Text="30 minutes",Value="30m" },
-                                new BitTimeframe {Text="one hour",Value="1h" },
-                                new BitTimeframe {Text="3 hours",Value="3h" },
-                                new BitTimeframe {Text="6 hours",Value="6h" },
-                                new BitTimeframe {Text="12 hours",Value="12h" },
-                                new BitTimeframe {Text="one day",Value="1D" },
-                                new BitTimeframe {Text="one week",Value="7D" },
-                                new BitTimeframe {Text="two weeks",Value="14D" },
-                                new BitTimeframe {Text="one month",Value="1M" }
+            var timeFrame = new List<ClsTimeframe>() {
+                                new ClsTimeframe {Text="one minute",Value="1m" },
+                                new ClsTimeframe {Text="five minutes",Value="5m" },
+                                new ClsTimeframe {Text="15 minutes",Value="15m" },
+                                new ClsTimeframe {Text="30 minutes",Value="30m" },
+                                new ClsTimeframe {Text="one hour",Value="1h" },
+                                new ClsTimeframe {Text="3 hours",Value="3h" },
+                                new ClsTimeframe {Text="6 hours",Value="6h" },
+                                new ClsTimeframe {Text="12 hours",Value="12h" },
+                                new ClsTimeframe {Text="one day",Value="1D" },
+                                new ClsTimeframe {Text="one week",Value="7D" },
+                                new ClsTimeframe {Text="two weeks",Value="14D" },
+                                new ClsTimeframe {Text="one month",Value="1M" }
+            };
+            return timeFrame;
+        }
+        public static List<ClsTimeframe> GetMTTimeFrame()
+        {
+            var timeFrame = new List<ClsTimeframe>() {
+                        new ClsTimeframe {Text="CURRENT ",Value="0" },
+                        new ClsTimeframe {Text="M1",Value="1" },
+                        new ClsTimeframe {Text="M2",Value="2" },
+                        new ClsTimeframe {Text="M3",Value="3" },
+                        new ClsTimeframe {Text="M4",Value="4" },
+                        new ClsTimeframe {Text="M5",Value="5" },
+                        new ClsTimeframe {Text="M15",Value="15" },
+                        new ClsTimeframe {Text="M30",Value="30" },
+                        new ClsTimeframe {Text="H1",Value="60" },
+                        new ClsTimeframe {Text="H4",Value="240" },
+                        new ClsTimeframe {Text="D1",Value="1440" },
+                        new ClsTimeframe {Text="W1",Value="10080" },
+                        new ClsTimeframe {Text="MN1",Value="43200" },
+
             };
             return timeFrame;
         }
